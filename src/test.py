9c9a01e0899c6bab834b3492058b91616cad6c69
nirvguy <nirvguy@gmail.com>
2016-06-25 21:46:06 -0300

    Actualiza los tests para chequear aritmetica y logica

--- src/test.py
+++ src/test.py
@@ -10,12 +10,12 @@ parser = yacc(module=parser_rules)
 # text = "qwe=1;qwe=2;sarasa(qwe[2+3+(4+5)*6+7]);"
 text = """
 qwe=1.123;asd=["qwe","zxc"]; # mira! comentario!; esto="no anda"; # comentario dentro de comentario?
-zxc=0.0;
+zxc=true AND true OR false;
 a=[];
 b=[1, 1.0];
-c={marto:"es",puto:true};
-if(a)qwe=1.0;qwe=2.0;
-if(a)qwe=1.0;else zxc=1.0;
+c={marto:"es",puto: TRUE, edad: 10 + 13};
+if(a)qwe=2^3+4;qwe=(2.0*4+3.0)^3;
+if(a)qwe=1.0/3;else zxc=1.0;
 if(a){qwe=1.0;zxc=1;if(b)qwe=2.0;}else qwe=0.0;
 if(a){qwe=1.0;zxc=1;if(b)qwe=2.0;}else{qwe=0.0;asd=3.0;}
 for(i=0;i;i)qwe=2.0;
